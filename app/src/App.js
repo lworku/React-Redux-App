733dee93528e357e33bd826001fb5fd6c66f15f0
lworku <lydiasgthb@gmail.com>
2020-09-24 19:10:39 -0700

    mvp

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+// import Characters from "./components/HpCharacter";
+import HpCharacter from './Components/HpCharacter';
+
+
+function App() {
+  return (
+    <div className = "App">
+      <HpCharacter /> 
+    </div>
+  )};
+
+export default App;
